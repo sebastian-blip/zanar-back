8bc621acb1d3e606e3c77d73f6f4683a057290bf
Kingsbd <danireyes_2@hotmail.com>
2021-05-07 22:16:45 -0500

    Given the limitations of heroku, the way the server runs is changed

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
 import {} from 'dotenv/config';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import helmet from 'helmet';
-import cors from 'cors';
+// import express from 'express';
+import { ApolloServer } from 'apollo-server';
+// import bodyParser from 'body-parser';
+// import mongoose from 'mongoose';
+// import helmet from 'helmet';
+// import cors from 'cors';
 import { sequelize } from './database/mySql';
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-const app = express();
+// const app = express();
 
-const arrAllowedOrigins = [
-	'http://localhost:7000',
-	'http://localhost:3440',
-	'http://localhost:4500',
-	'http://localhost:4000'
-];
+// const arrAllowedOrigins = [
+// 	'http://localhost:7000',
+// 	'http://localhost:3440',
+// 	'http://localhost:4500',
+// 	'http://localhost:4000'
+// ];
 
-const corsOptions = {
-	origin(origin, callback) {
-		if (arrAllowedOrigins.indexOf(origin) !== -1 || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	}
-};
+// const corsOptions = {
+// 	origin(origin, callback) {
+// 		if (arrAllowedOrigins.indexOf(origin) !== -1 || !origin) {
+// 			callback(null, true);
+// 		} else {
+// 			callback(new Error('Not allowed by CORS'));
+// 		}
+// 	}
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-app.use(helmet());
+// app.use(helmet());
 
-app.use(bodyParser.json({ limit: '8mb' }));
+// app.use(bodyParser.json({ limit: '8mb' }));
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
 });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
 sequelize
 	.sync()
 	.then(() => {
 		try {
 			const port = process.env.PORT || 4500;
-			app.listen({ port }, () => {
+			server.listen({ port }, () => {
 				console.log(`Server is working on localhost:4500${server.graphqlPath}`);
 			});
 		} catch (errServer) {
